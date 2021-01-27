d80263ed9bdd3b1c57e6ca160b9823dd5cf8f4ea
Marten Veldmann <marten.veldmann@dzne.de>
2021-01-27 09:49:48 +0100

    add first working Python wrapper with boost.python

---
+++ PowerGrid/PowerGridPy.cpp
@@ -0,0 +1,283 @@
+
+/*
+(C) Copyright 2015-2016 The Board of Trustees of the University of Illinois.
+All rights reserved.
+
+See LICENSE.txt for the University of Illinois/NCSA Open Source license.
+
+Developed by:
+                     MRFIL Research Groups
+                University of Illinois, Urbana-Champaign
+*/
+
+/*****************************************************************************
+
+    File Name   [PowerGridIsmrmrd.cpp]
+
+    Synopsis    [PowerGrid reconstruction executable supporting ISMRMRD format
+                                as input.]
+
+    Description [This reconstruction supports 2D and 3D reconstructions with
+                                time segmentation for field correction. This
+ support is experimental.]
+
+    Revision    [0.2.0; Alex Cerjanic, BIOE UIUC]
+
+    Date        [2019/03/31]
+
+ *****************************************************************************/
+
+// //Project headers.
+#include "PowerGrid.h"
+#include "processIsmrmrd.hpp"
+#include "processNIFTI.hpp"
+#include <boost/program_options.hpp>
+#include <chrono>
+
+#include <boost/python.hpp>
+
+//using namespace PowerGrid;
+
+int PowerGridIsmrmrd(std::string inFile, std::string outFile, int nx, int ny, int nz, int nShots, std::string  TSInterp,
+                     std::string FourierTrans, int timesegs, double beta, int iter, int regDims) {
+
+  uword Nx = nx;
+  uword Ny = ny;
+  uword Nz = nz;
+  uword NShots = nShots;
+  uword L = timesegs;
+  uword NIter = iter;
+  uword dims2penalize = regDims;
+  uword FtType = 1, type = 1;
+  if (FourierTrans.compare("DFT") == 0) {
+      FtType = 2;
+  
+  } else if (FourierTrans.compare("NUFFT") == 0) {
+    
+    FtType = 1;
+
+    if (TSInterp.compare("hanning") == 0) {
+      type = 1;
+    } else if (TSInterp.compare("minmax") == 0) {
+      type = 2;
+    } else if (TSInterp.compare("histo") == 0) {
+      type = 3;
+    } else {
+      std::cout << "Did not recognize temporal interpolator selection. " << std::endl
+                << "Acceptable values are hanning or minmax."            << std::endl;
+      return 1;
+    }
+  
+  } else if (FourierTrans.compare("DFTGrads") == 0) {
+    FtType = 3;
+  } else {
+    std::cout << "Did not recognize Fourier transform selection. " << std::endl
+              << "Acceptable values are DFT or NUFFT."             << std::endl;
+    return 1;
+  }
+
+
+  ISMRMRD::Dataset *d;
+  ISMRMRD::IsmrmrdHeader hdr;
+  acqTracking *acqTrack;
+  Col<float> FM;
+  Col<std::complex<float>> sen;
+  processISMRMRDInput<float>(inFile, d, hdr, FM, sen, acqTrack);
+
+  //std::cout << "Number of elements in SENSE Map = " << sen.n_rows << std::endl;
+  //std::cout << "Number of elements in Field Map = " << FM.n_rows << std::endl;
+
+  uword numAcq = d->getNumberOfAcquisitions();
+
+  // Grab first acquisition to get parameters (We assume all subsequent
+  // acquisitions will be similar).
+  ISMRMRD::Acquisition acq;
+  d->readAcquisition(numAcq-1, acq); // take last acquisition as first acquisition is usually a noise scan - mv
+  uword nro = acq.number_of_samples();
+  uword nc = acq.active_channels();
+
+  	// Handle Nx, Ny, Nz
+	if(Nx==0) {
+		Nx = hdr.encoding[0].encodedSpace.matrixSize.x;
+	} 
+	if(Ny==0) {
+		Ny = hdr.encoding[0].encodedSpace.matrixSize.y;
+	} 
+	if(Nz==0) {
+		Nz = hdr.encoding[0].encodedSpace.matrixSize.z;
+	} 
+
+  Col<float> ix, iy, iz;
+  initImageSpaceCoords(ix, iy, iz, Nx, Ny, Nz);
+
+
+  // Check and abort if we have more than one encoding space (No Navigators for
+  // now).
+  std::cout << "hdr.encoding.size() = " << hdr.encoding.size() << std::endl;
+  if (hdr.encoding.size() != 1) {
+    std::cout << "There are " << hdr.encoding.size()
+              << " encoding spaces in this file" << std::endl;
+    std::cout
+        << "This recon does not handle more than one encoding space. Aborting."
+        << std::endl;
+    return -1;
+  }
+
+  int NShotMax  = hdr.encoding[0].encodingLimits.kspace_encoding_step_1->maximum;
+  int NParMax   = hdr.encoding[0].encodingLimits.kspace_encoding_step_2->maximum;
+  int NSliceMax = hdr.encoding[0].encodingLimits.slice->maximum;
+  int NSetMax   = hdr.encoding[0].encodingLimits.set->maximum;
+  int NRepMax   = hdr.encoding[0].encodingLimits.repetition->maximum;
+  int NAvgMax   = hdr.encoding[0].encodingLimits.average->maximum;
+  int NSegMax   = hdr.encoding[0].encodingLimits.segment->maximum;
+  int NEchoMax  = hdr.encoding[0].encodingLimits.contrast->maximum;
+  int NPhaseMax = hdr.encoding[0].encodingLimits.phase->maximum;
+
+  std::cout << "NParMax = "   << NParMax << std::endl;
+  std::cout << "NShotMax = "  << NShotMax << std::endl;
+  std::cout << "NSliceMax = " << NSliceMax << std::endl;
+  std::cout << "NSetMax = "   << NSetMax << std::endl;
+  std::cout << "NRepMax = "   << NRepMax << std::endl;
+  std::cout << "NAvgMax = "   << NAvgMax << std::endl;
+  std::cout << "NEchoMax = "  << NEchoMax << std::endl;
+  std::cout << "NPhaseMax = " << NPhaseMax << std::endl;
+  std::cout << "NSegMax = "   << NSegMax << std::endl;
+
+   std::cout << "About to loop through the counters and scan the file"
+            << std::endl;
+
+  // std::string baseFilename = "img";
+
+  
+  std::string filename;
+  // if (!outputImageFilePath.empty() && *outputImageFilePath.rbegin() != '/') {
+  //   	outputImageFilePath += '/';
+	// }
+
+  for (uword NSlice = 0; NSlice<=NSliceMax; NSlice++) {
+  
+    //acc_set_device_num(tid, acc_device_nvidia);
+    //Col<float> FM;
+    Col<float> fmSlice;
+    //Col<std::complex<float>> sen;
+ 	Col<std::complex<float>> senSlice;
+    Col<float> kx(nro), ky(nro), kz(nro), tvec(nro);
+    Col<std::complex<float>> data(nro * nc);
+    Col<std::complex<float>> ImageTemp(Nx * Ny * Nz);
+
+	  for (uword NPhase = 0; NPhase <= NPhaseMax; NPhase++) {
+		for (uword NEcho = 0; NEcho <= NEchoMax; NEcho++) {
+          for (uword NAvg = 0; NAvg <= NAvgMax; NAvg++) {
+             for (uword NRep = 0; NRep < NRepMax +1; NRep++) {
+
+
+                      filename = outFile + "_" + "Slice" + std::to_string(NSlice) +
+          								"_" + "Rep" + std::to_string(NRep) + "_" + "Avg" + std::to_string(NAvg) +
+          								"_" + "Echo" + std::to_string(NEcho) + "_" + "Phase" + std::to_string(NPhase);
+
+	                      senSlice = getISMRMRDCompleteSENSEMap<std::complex<float>>(d, sen, NSlice, Nx*Ny*Nz);
+						            fmSlice = getISMRMRDCompleteFieldMap<float>(d, FM, NSlice, (uword) (Nx*Ny*Nz));
+	                      getCompleteISMRMRDAcqData<float>(d, acqTrack, NSlice, NRep, NAvg, NEcho, NPhase, data, kx, ky,
+			                    kz, tvec);
+
+                          // Deal with the number of time segments
+						            if(L==0) {
+							            switch(type) {
+								            case 1:
+									            L = ceil((arma::max(tvec) - arma::min(tvec))/2E-3);
+								            break;
+								            case 2:
+									            L = ceil((arma::max(tvec) - arma::min(tvec))/3E-3);
+								            break;
+								            case 3:
+									            L = ceil((arma::max(tvec) - arma::min(tvec))/3E-3);
+								            break;
+								            default: 
+									          L = 0;
+							            }
+							            std::cout << "Info: Setting L = " << L << " by default." << std::endl; 
+						            }
+
+
+	                    std::cout << "Number of elements in kx = " << kx.n_rows << std::endl;
+	                    std::cout << "Number of elements in ky = " << ky.n_rows << std::endl;
+	                    std::cout << "Number of elements in kz = " << kz.n_rows << std::endl;
+	                    std::cout << "Number of rows in data = " << data.n_rows << std::endl;
+	                    std::cout << "Number of columns in data = " << data.n_cols << std::endl;
+
+	                    QuadPenalty<float> R(Nx, Ny, Nz, beta, dims2penalize);
+
+                      if (FtType == 1) {
+	                      Gnufft<float> G(kx.n_rows, (float) 2.0, Nx, Ny, Nz, kx, ky, kz, ix,
+			                    iy, iz);
+	                      TimeSegmentation<float, Gnufft<float>> A(G, fmSlice, tvec, kx.n_rows, Nx*Ny*Nz, L, type, NShots);
+                        SENSE<float, TimeSegmentation<float, Gnufft<float>>> Sg(A, senSlice, kx.n_rows, Nx*Ny*Nz, nc);
+	                      ImageTemp = reconSolve<float, SENSE<float, TimeSegmentation<float, Gnufft<float>>>,
+			                    QuadPenalty<float>>(data, Sg, R, kx, ky, kz, Nx,
+			                    Ny, Nz, tvec, NIter);
+                      } else if (FtType == 2) {
+                        Gdft<float> A(kx.n_rows, Nx*Ny*Nz,kx,ky,kz,ix,iy,iz,fmSlice,tvec);
+	                      SENSE<float, Gdft<float>> Sg(A, senSlice, kx.n_rows, Nx*Ny*Nz, nc);
+	                      ImageTemp = reconSolve<float, SENSE<float, Gdft<float>>,
+	                            QuadPenalty<float>>(data, Sg, R, kx, ky, kz, Nx,
+                              Ny, Nz, tvec, NIter);
+                      } else if (FtType == 3) {
+                        GdftR2<float> A(kx.n_rows, Nx*Ny*Nz,kx,ky,kz,ix,iy,iz,fmSlice,tvec,Nx,Ny,Nz);
+	                      SENSE<float, GdftR2<float>> Sg(A, senSlice, kx.n_rows, Nx*Ny*Nz, nc);
+	                      ImageTemp = reconSolve<float, SENSE<float, GdftR2<float>>,
+	                            QuadPenalty<float>>(data, Sg, R, kx, ky, kz, Nx,
+                              Ny, Nz, tvec, NIter);
+                      }
+
+
+	                  //writeISMRMRDImageData<float>(d, ImageTemp, Nx, Ny, Nz);
+                    writeNiftiMagPhsImage<float>(filename,ImageTemp,Nx,Ny,Nz);
+                    }
+
+                }
+            }
+        }
+    }
+
+
+  // Close ISMRMRD::Dataset, hdr, and acqTrack
+	closeISMRMRDData(d,hdr,acqTrack);
+
+  return 0;
+}
+
+BOOST_PYTHON_MODULE(PowerGridPy) {
+
+  namespace python = boost::python;
+  python::docstring_options doc_options;
+  doc_options.disable_py_signatures();
+  doc_options.disable_cpp_signatures();
+  // Expose the function PowerGrid().
+  python::def("PowerGridIsmrmrd", PowerGridIsmrmrd, 
+              "Executes PowerGridIsmrmrd with specified parameters.\n\\
+                Parameters\n\\
+                ----------\n\\
+                    inFile : string\n\\
+                        input ISMRMRD Raw Data file (incl path)\n\\
+                    outFile : string\n\\
+                        output ISMRMRD Raw Data file (incl path)\n\\
+                    FourierTrans : string\n\\
+                        Fourier Transform (NUFFT (default), DFT or DFTGrads)\n\\
+                    timesegs : int\n\\
+                        Number of time segments for B0 correction. Default is 0, then time segments will be determined from readout duration.\n\\
+                    iter: int\n\\
+                        Number of CG iterations (default=10)\n\\
+                    TSInterp: string\n\\
+                        Field Correction Interpolator (histo (default), hanning or minmax)\n\\
+                    beta: float\n\\
+                        Spatial regularization penalty weight (default=0)\n\\
+                    regDims: int\n\\
+                        Dimensions to apply regularization to (2 or 3 (default))\n\\
+                    nShots: int\n\\
+                        Number of Shots (default: 1)\n\\
+                    nx, ny, nz : int\n\\
+                        Image size in x,y,z. Default is 0, then sizes are read from ISRMRD header encoded space.",
+                (python::arg("inFile"), "outFile", python::arg("nx")=0, python::arg("ny")=0, python::arg("nz")=0, python::arg("nShots")=1, python::arg("TSInterp")="histo",
+                 python::arg("FourierTrans")="NUFFT", python::arg("timesegs")=0, python::arg("beta")=0.0, python::arg("iter")=10, python::arg("regDims")=3)
+        );
+}
---
+++ info_mv.md
@@ -0,0 +1,78 @@
+# PowerGrid Compiling & Debugging Infos
+
+## Compiling
+
+For Compiling install packages on https://github.com/mrfil/PowerGrid.  
+Especially Armadillo has to be version 8.600.x or 9.200.x (only available in Docker Container of PowerGrid).  
+Compiling is working with newer versions of ISMRMRD (e.g. 1.4.2).  
+Cmake should be compiled with OPENACC_GPU=OFF und OPENACC_MP=ON/OFF if no CUDA is available (change in CMakeLists.txt).  
+
+For Compiling:  
+mkdir build  
+cd build  
+cmake ..  
+make  
+sudo make install  
+
+## Compile Python wrapper
+
+Just run `pip install .` from the PowerGrid root directory. This installs the module PowerGridPy, from which we can call PowerGridIsmrmrd.
+
+## Debugging in VS Code
+
+in tasks.json:
+
+```
+{
+    "version": "2.0.0",
+    "tasks": [
+        {
+            "label": "build",
+            "type": "shell",
+            "command": "cd build && cmake .. && make",
+            "problemMatcher": [
+                "$gcc"
+            ]
+        }
+    ]
+}
+```
+
+in launch.json (Dateien jeweils ersetzen):
+
+```
+{
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "(gdb) Starten",
+            "type": "cppdbg",
+            "request": "launch",
+            "program": "/home/veldmannm/Software/PowerGrid/build/PowerGridIsmrmrd",
+            "args": [
+                "-i",
+                "/home/veldmannm/Projects/SpiralGirfProject/RawData/20201210/in_vivo/power_grid_test_MID251.h5",
+                "-o",
+                "/home/veldmannm/Projects/SpiralGirfProject/RawData/20201210/in_vivo/powergridimages",
+                "-F",
+                "NUFFT",
+                "-t",
+                "1",
+                "-B",
+                "0.1"],
+            "stopAtEntry": false,
+            "cwd": "${workspaceFolder}",
+            "environment": [],
+            "externalConsole": false,
+            "MIMode": "gdb",
+            "setupCommands": [
+                {
+                    "description": "Automatische Strukturierung und Einrückung für \"gdb\" aktivieren",
+                    "text": "-enable-pretty-printing",
+                    "ignoreFailures": true
+                }
+            ]
+        }
+    ]
+}
+```
